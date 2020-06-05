bafbedf4bd24a59b9755ebc89ec948fd49eda909
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-06-05 15:34:57 -0300

    Create 7_kyu_Sum_of_two_lowest_positive_integers.py

---
+++ 7_kyu_Sum_of_two_lowest_positive_integers.py
@@ -0,0 +1,17 @@
+'''
+Instructions
+
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+'''
+
+#CODE
+def sum_two_smallest_numbers(numbers):
+    mins = []
+    for i in range(2):
+        mins.append(min(numbers))
+        numbers.remove(min(numbers))
+    return sum(mins)
