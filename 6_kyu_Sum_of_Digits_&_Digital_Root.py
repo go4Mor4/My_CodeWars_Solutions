5da2aa30d77b468aa77847f361e997b607400a93
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-06-05 15:37:21 -0300

    Create 6_kyu_Sum_of_Digits_&_Digital_Root.py

---
+++ 6_kyu_Sum_of_Digits_&_Digital_Root.py
@@ -0,0 +1,24 @@
+'''
+Instructions
+
+Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+
+Examples
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+'''
+
+#CODE
+def digital_root(n):
+  n = ''.join(str(n))
+  while len(n) != 1:
+    n = somar(n)
+  return int(n)
+def somar(n):
+  n = ' '.join(str(n))
+  n = list(map(int, n.split(' ')))
+  return ''.join(str(sum(n)))
