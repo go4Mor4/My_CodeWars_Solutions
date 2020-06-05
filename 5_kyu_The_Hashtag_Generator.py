c64b40e8225e60f16e27bde6fe7f25afb5a1a741
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-06-05 15:31:22 -0300

    Create 5_kyu_The_Hashtag_Generator.py

---
+++ 5_kyu_The_Hashtag_Generator.py
@@ -0,0 +1,29 @@
+'''
+Instructions
+
+The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+It must start with a hashtag (#).
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.
+Examples
+
+" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+"    Hello     World   "                  =>  "#HelloWorld"
+""                                        =>  false
+'''
+
+#CODE
+def generate_hashtag(string):
+    frase = '#'
+    lista = string.split()
+    for i in range(len(lista)):
+        palavra = ''.join(lista[i])
+        palavra = palavra.lower().capitalize()
+        frase += palavra
+    if len(frase) >= 140 or len(frase) - 1 == 0: return False
+    else: return frase
