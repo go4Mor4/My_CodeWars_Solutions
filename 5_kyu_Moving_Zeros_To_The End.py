7b3d3746e3e31ff47cd26c57590596f28e677e49
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-06-05 15:28:01 -0300

    Update 5_kyu_Moving_Zeros_To_The End.py

--- 5_kyu_Moving_Zeros_To_The End.py
+++ 5_kyu_Moving_Zeros_To_The End.py
@@ -9,15 +9,11 @@ move_zeros([false,1,0,1,2,0,1,3,"a"]) # returns[false,1,1,2,1,3,"a",0,0]
 def move_zeros(array):
   n_zeros = 0
   for i in range(len(array)):
-      if array[i] is False:
-        array[i] = 'False'
+      if array[i] is False: array[i] = 'False'
   for i in array:
-      if i == 0:
-          n_zeros += 1
+      if i == 0: n_zeros += 1
   array = [value for value in array if value != 0]
-  for i in range(n_zeros):
-      array.append(0)
+  for i in range(n_zeros): array.append(0)
   for i in range(len(array)):
-      if array[i] == 'False':
-        array[i] = False
+      if array[i] == 'False': array[i] = False
   return array
